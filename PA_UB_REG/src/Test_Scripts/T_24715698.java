40a2b5a299596f25199b5777e1a27317db30987e
saich <saich@DESKTOP-RQB5J1P>
2018-03-15 13:17:52 +0530

    Test Commit

---
+++ PA_UB_REG/src/Test_Scripts/T_24715698.java
@@ -0,0 +1,20 @@
+
+package Test_Scripts;
+
+
+
+
+
+public class T_24715698 {
+	public static void main(String args[]) throws Exception{
+		
+	
+	Runtime.getRuntime().exec("D:\\PAUB\\Login_Mobaxterm.exe");
+	//Runtime.getRuntime().exec("D:\\PAUB\\VM.exe");
+	
+		
+		
+	}}
+
+
+
